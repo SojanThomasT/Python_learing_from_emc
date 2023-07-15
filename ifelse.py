5ab5711ed7dcf89a1e51c52165593f44d2156610
SojanThomasT <tsojanthomas@gmail.com>
2023-07-15 14:33:48 +0530

    third commit

--- ifelse.py
+++ ifelse.py
@@ -31,3 +31,41 @@ if a%3 == 0:
     print("the number is divisible by 3")
 else:
     print("the number is not divisible by 3")
+
+# get a number and find wheathere is devisibile by both:
+num = int(input("Enter a Number: "))
+if (num%5 == 0) and (num%3 == 0): #(num%5 and num%3) == 0:
+    print(num, "is divisible by 3 and 5")
+else:
+    print(num, "this is not divisible by 3 and 5")
+
+#get input and even or odd:
+num = int(input())
+if (num%2) == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
+
+#get a score from user and apply condition:
+score = int(input())
+if score < 35:
+    print("Poor Student")
+elif 35<=score<70:
+    print("Average Student")
+elif score>=70:
+    print("Good Student")
+
+#mini calculator:
+a = int(input())
+b = int(input())
+option = input("choose a operation add/sub/mul/div:  ")
+if option.lower() == "add":
+    print(a + b)
+elif option.lower() == "sub":
+    print(a-b)
+elif option.lower() == "mul":
+    print(a*b)
+elif option.lower() == "div":
+    print(a/b)
+else:
+    print("Enter Valid Option")
