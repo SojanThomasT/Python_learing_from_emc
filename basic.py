53094490da88db9cde4f63cd0f61d450d1f90a9c
SojanThomasT <tsojanthomas@gmail.com>
2023-07-15 12:26:58 +0530

    second commit

---
+++ basic.py
@@ -0,0 +1,29 @@
+#get uinput from user and  print it
+name = input()
+age = int(input())
+address = input()
+#o/p state ment
+print("My name is:" + name)
+print("My age is:" + str(age))
+print("My address:" + address)
+
+
+#get three input from use and add it and then multiply it
+a=int(input())
+b=int(input())
+c=int(input())
+d=(a*b*c)
+e=(a+b+c)
+f=d/e
+print(f)
+
+
+#get input from user as name, mark 
+# and department then convert mark for 1o ath then pic 
+name = input()
+mark = int(input())
+department = input()
+mark_for_10 = float(mark/10) 
+print("My Name is" , name)
+print("My mark is" , mark_for_10 ,"/10")
+print("My Department is" , department)
---
+++ ifelse.py
@@ -0,0 +1,33 @@
+
+rcb ="win"
+if (rcb =="win"):
+    print("RCB won the match")
+else:
+    print("RCB loss the match again")
+
+user =input()
+if (user.lower() == "died"):
+    print("Surya Meets Priya")
+else:
+    print("Surya weds Megna ")
+
+mark = int(input("Enter Your Mark: "))
+if mark >=35:
+    print("You passed :)")
+else:
+    print("Sorry! You Failed :|")
+
+    
+income= int(input("Enter your Income: "))
+if income >= 7000:
+    print("Your are Not Eligible for scholarship")
+else:
+    print("Your are Eligible for scholarship")
+
+
+
+a = int(input("Enter a number"))
+if a%3 == 0:
+    print("the number is divisible by 3")
+else:
+    print("the number is not divisible by 3")
