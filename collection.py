11ced3c300631e4d37e54e1c705fb3512a6da7d6
SojanThomasT <tsojanthomas@gmail.com>
2023-07-18 15:30:01 +0530

    new

---
+++ collection.py
@@ -0,0 +1,4 @@
+#list[]-------> allow duplicate| all data type accepted| can modify  a[0]=1 |can add or remove   | insert(),append(),extend(),pop()
+#tuple()------> allow duplicate| all data type accepted| cannot modify      |cannot add or remove| nope
+#set{}--------> no duplicate   | all data type accepted| cannot modify      |can add or remove   | add(),update(),remove(),pop()
+#dictinary{}--> no duplicate   | all data type accepted| can modify         |
--- whileloop.py
+++ whileloop.py
@@ -1,4 +1,4 @@
-"""
+
 #example:
 i=0
 while(i==0):
@@ -9,5 +9,23 @@ i=1
 while(i<5):
     print(i)
     i+=1
-"""
-    
+
+# print a avlue btw 100 to 200:
+i=10
+while(i<=200):
+    print(i,end=",")
+    i+=10 
+
+#print 1 to 10 in reverse order:
+i=10
+while(i>0):
+    print(i, end=",")
+    i-=1
+
+#write program to find factorial
+num=int(input())
+fact=1
+while(num>0):
+    fact*=num
+    num-=1
+print(fact)
